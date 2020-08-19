9116b797b1db7d1e4e7be427d5922cfde063872a
rosvali <rosval6578@gmail.com>
2020-08-20 00:33:13 +0200

    add 4 and 5

---
+++ script_4.js
@@ -0,0 +1,69 @@
+const entrepreneurs = [
+  { first: 'Steve', last: 'Jobs', year: 1955 },
+  { first: 'Oprah', last: 'Winfrey', year: 1954 },
+  { first: 'Bill', last: 'Gates', year: 1955 },
+  { first: 'Sheryl', last: 'Sandberg', year: 1969 },
+  { first: 'Mark', last: 'Zuckerberg', year: 1984 },
+  { first: 'Beyonce', last: 'Knowles', year: 1981 },
+  { first: 'Jeff', last: 'Bezos', year: 1964 },
+  { first: 'Diane', last: 'Hendricks', year: 1947 },
+  { first: 'Elon', last: 'Musk', year: 1971 },
+  { first: 'Marissa', last: 'Mayer', year: 1975 },
+  { first: 'Walt', last: 'Disney', year: 1901 },
+  { first: 'Larry', last: 'Page', year: 1973 },
+  { first: 'Jack', last: 'Dorsey', year: 1976 },
+  { first: 'Evan', last: 'Spiegel', year: 1990 },
+  { first: 'Brian', last: 'Chesky', year: 1981 },
+  { first: 'Travis', last: 'Kalanick', year: 1976 },
+  { first: 'Marc', last: 'Andreessen', year: 1971 },
+  { first: 'Peter', last: 'Thiel', year: 1967 }
+];
+
+/**** Question 1 ****/
+
+console.log("Filtre dans cette liste les entrepreneurs qui sont nés dans les années 70");
+
+for (let index in entrepreneurs) {
+  if (entrepreneurs[index].year > 1969 && entrepreneurs[index].year < 1981) {
+    console.log(entrepreneurs[index]);
+  }
+}
+
+/**** Question 2 ****/
+
+console.log("Sors une array qui contient le prénom et le nom des entrepreneurs");
+
+let names = []
+
+entrepreneurs.forEach(entrepreneur => {
+  names.push(entrepreneur.first + " " + entrepreneur.last);
+});
+
+console.log(names);
+
+/**** Question 3 ****/
+
+console.log("Quel âge aurait chaque inventeur aujourd'hui ?");
+
+entrepreneurs.forEach(entrepreneur => {
+  console.log(`${entrepreneur.first} ${entrepreneur.last} aurait aujourd'hui ${2020 - entrepreneur.year} ans`);
+});
+
+/**** Question 4 ****/
+
+console.log("Trie les entrepreneurs par ordre alphabétique du nom de famille");
+
+function compare(a, b) {
+  const lastA = a.last;
+  const lastB = b.last;
+
+  let comparison = 0;
+  if (lastA > lastB) {
+    comparison = 1;
+  } else if (lastA < lastB) {
+    comparison = -1;
+  }
+  return comparison;
+}
+
+console.log(entrepreneurs.sort(compare));
---
+++ script_5.js
@@ -0,0 +1,107 @@
+const books = [
+  { title: 'Gatsby le magnifique', id: 133712, rented: 39 },
+  { title: 'A la recherche du temps,perdu', id: 237634, rented: 28 },
+  { title: 'Orgueil & Préjugés', id: 873495, rented: 67 },
+  { title: 'Les frères Karamazov', id: 450911, rented: 55 },
+  { title: 'Dans les forêts de Sibérie', id: 8376365, rented: 15 },
+  { title: 'Pourquoi j\'ai mangé mon père', id: 450911, rented: 45 },
+  { title: 'Et on tuera tous les affreux', id: 67565, rented: 36 },
+  { title: 'Le meilleur des mondes', id: 88847, rented: 58 },
+  { title: 'La disparition', id: 364445, rented: 33 },
+  { title: 'La lune seule le sait', id: 63541, rented: 43 },
+  { title: 'Voyage au centre de la Terre', id: 4656388, rented: 38 },
+  { title: 'Guerre et Paix', id: 748147, rented: 19 },
+  // { title: 'Tony Chu', id: 123456, rented: 0 }
+];
+
+/* **** Question 1 **** */
+
+console.log("Est-ce que tous les livres ont été au moins empruntés une fois ?");
+
+function rent()
+{
+  for (let index in books) {
+    if (books[index].rented == 0) {
+      return false;
+    }
+  };
+  return true;
+}
+
+console.log(rent());
+
+/* **** Question 2 **** */
+
+console.log("Quel est livre le plus emprunté ?");
+
+function compare_rent(a, b) {
+  const rentedA = a.rented;
+  const rentedB = b.rented;
+
+  let comparison = 0;
+  if (rentedA > rentedB) {
+    comparison = 1;
+  } else if (rentedA < rentedB) {
+    comparison = -1;
+  }
+  return comparison;
+}
+
+console.log(books.sort(compare_rent)[books.length - 1])
+
+/* **** Question 3 **** */
+
+console.log("Quel est le livre le moins emprunté ?");
+
+console.log(books.sort(compare_rent)[0])
+
+/* **** Question 4 **** */
+
+console.log("Trouve le livre avec l'ID: 873495");
+
+function find_book(id) {
+  for (let index in books) {
+    if (books[index].id == id) {
+      return books[index];
+    }
+  };
+  return "Book not found";
+}
+
+console.log(find_book(873495));
+
+/* **** Question 5 **** */
+
+console.log("Supprime le livre avec l'ID: 133712");
+
+function rm_book(id) {
+  for (let index in books)
+    if (books[index].id == id) {
+      console.log(`${books[index].title} deleted`);
+      delete books[index]
+    };
+}
+
+rm_book(133712);
+
+/* **** Question 6 **** */
+
+console.log("Trie les livres par ordre alphabétique");
+
+function compare_title(a, b) {
+  const titleA = a.title;
+  const titleB = b.title;
+
+  let comparison = 0;
+  if (titleA > titleB) {
+    comparison = 1;
+  } else if (titleA < titleB) {
+    comparison = -1;
+  }
+  return comparison;
+}
+
+let new_books = books.sort(compare_title);
+new_books.pop();
+console.log(new_books);
+
